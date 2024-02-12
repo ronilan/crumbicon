4a1c0cde540f2a526fb8e7e4ceed238f8d9091f2
Ron Ilan <ronilan.webdev@gmail.com>
2024-02-11 17:12:40 -0800

    Fixed build script to correctly account for build errors.

--- build-crumb.sh
+++ build-crumb.sh
@@ -4,7 +4,14 @@ checkout=${1:-$default}
 into=".tmp~crumb~inflate"
 
 build_and_clean() {
-  gcc src/*.c -Wall -lm -o crumb
+
+if gcc src/*.c -Wall -lm -o crumb;
+then
+  printf "\e[32mCrumb built successfully\e[0m\n"
+else
+  printf "\e[31mCrumb built failed\e[0m\n"
+  exit 1
+fi
   cd ..
   mv ./$into/crumb crumb
   rm -rf $into
@@ -17,14 +24,12 @@ then
   cd $into || exit
   git reset --hard "$checkout"
   build_and_clean
-  printf "\e[32mCrumb built successfully from %s\e[0m\n" "$checkout"
 elif [ "$(printf '%s' "$checkout" | cut -c 1)" = "v" ] # it is a sha
 then # assumed a version tag
   printf "\e[32mBuilding version %s \e[0m \n" "$checkout"
   git clone -c advice.detachedHead=false --branch "$checkout" --depth 1 https://github.com/liam-ilan/crumb.git $into
   cd $into || exit
   build_and_clean
-  printf "\e[32mCrumb version %s built successfully\e[0m\n" "$checkout"
 else
   printf "\e[31m%s not valid version tag nor SHA \e[0m\n" "$checkout"
 fi
